dbe1dda9c5ca05a356f721744ad9c1c17c0df46e
다혜 <hdhzzv@gmail.com>
2021-12-07 17:33:51 +0900

    naver로그인 기능 구현 완료

--- nadri-spring/src/main/java/com/nadri/bo/NaverLoginBO.java
+++ nadri-spring/src/main/java/com/nadri/bo/NaverLoginBO.java
@@ -1,18 +1,28 @@
 package com.nadri.bo;
 
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 
+
+import org.apache.tomcat.util.buf.StringUtils;
+
 import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.model.Verb;
 import com.github.scribejava.core.oauth.OAuth20Service;
 import com.nadri.api.NaverService;
 
 public class NaverLoginBO {
 	private final static String CLIENT_ID = "SR3YR1kOQAMN7qDKjv8s"; //Client ID
     private final static String CLIENT_SECRET = "z67QvBqbov"; //Cliend Secret
-    private final static String REDIRECT_URI = "http://localhost:8088/main/index"; //로그인 성공시 url
+    private final static String REDIRECT_URI = "http://localhost:8088/user/callback"; //로그인 성공시 url
+    private final static String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";
     
     public String generateState() {
     	SecureRandom random = new SecureRandom();
@@ -34,4 +44,44 @@ public class NaverLoginBO {
     			.build(NaverService.instance());
     	return oauthService.getAuthorizationUrl();
     }
+    
+    /* 네아로 Callback 처리 및  AccessToken 획득 Method */
+	public OAuth2AccessToken getAccessToken(HttpSession session, String code, String state) throws IOException{
+		
+		/* Callback으로 전달받은 세선검증용 난수값과 세션에 저장되어있는 값이 일치하는지 확인 */
+		String sessionState =(String)session.getAttribute("state");
+		if(sessionState.equals(state)){
+		
+			OAuth20Service oauthService = new ServiceBuilder()
+					.apiKey(CLIENT_ID)
+					.apiSecret(CLIENT_SECRET)
+					.callback(REDIRECT_URI)
+					.state(state)
+					.build(NaverService.instance());
+					
+			/* Scribe에서 제공하는 AccessToken 획득 기능으로 네아로 Access Token을 획득 */
+			OAuth2AccessToken accessToken = oauthService.getAccessToken(code);
+			return accessToken;
+		}
+		return null;
+	}
+	
+	/* 세션 유효성 검증을 위한 난수 생성기 */
+	private String generateRandomString() {
+		return UUID.randomUUID().toString();
+	}
+	
+	/* Access Token을 이용하여 네이버 사용자 프로필 API를 호출 */
+	public String getUserProfile(OAuth2AccessToken oauthToken) throws IOException{
+
+		OAuth20Service oauthService =new ServiceBuilder()
+    			.apiKey(CLIENT_ID)
+    			.apiSecret(CLIENT_SECRET)
+    			.callback(REDIRECT_URI).build(NaverService.instance());
+    	
+    		OAuthRequest request = new OAuthRequest(Verb.GET, PROFILE_API_URL, oauthService);
+		oauthService.signRequest(oauthToken, request);
+		Response response = request.send();
+		return response.getBody();
+	}
 }
